7b74fc9de9534076188f82ce1a263857313e96f9
Jesus Reyes <luisdejesus200122@gmail.com>
2022-12-15 19:20:00 -0600

    Feat: Router Added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { HomePage } from './pages';
+import {  RouterProvider} from "react-router-dom";
+import { router } from './router/AppRouter';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HomePage />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
